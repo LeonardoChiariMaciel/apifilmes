e92bef3c03c2cabbb87b10f814e6713f4befdd6a
Leonardo <leo@lithera.com.br>
2025-04-13 16:41:06 -0300

    primeira tela

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const moviesRouter = require('./routes/movies');
 const app = express();
 const PORT = 3000;
-
-// Mock de dados para testar
-const movies = [
-  { title: 'Movie 1', poster_path: '/path1.jpg' },
-  { title: 'Movie 2', poster_path: '/path2.jpg' },
-];
 
 app.use(cors());
 app.use(express.json());
 
-// Rota para retornar filmes populares
-app.get('/api/movies/popular', (req, res) => {
-  const { type } = req.query; // 'type' vem da query string
-  res.json(movies); // Aqui estamos retornando os filmes mockados
-});
+app.use('/api/movies', moviesRouter);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
